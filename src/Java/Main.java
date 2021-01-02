85bea0c8faf76ce4ca5cd6dfaee2bcbb5f1d8ed8
ANDREW <1k2>
2021-01-02 23:02:46 +0300

    First test project

---
+++ src/Java/Main.java
@@ -0,0 +1,15 @@
+package Java;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+       Random random = new Random();
+       int a = random.nextInt(10);
+        System.out.println(a);
+
+
+    }
+
+}
+
